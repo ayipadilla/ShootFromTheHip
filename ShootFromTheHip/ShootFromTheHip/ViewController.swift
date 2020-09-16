89ee730918c4c13318f6448d656ec625d253de35
ayipadilla <danyelli@gmail.com>
2020-09-16 21:20:44 +0800

    Added initial project

---
+++ ShootFromTheHip/ShootFromTheHip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShootFromTheHip
+//
+//  Created by Ayi Padilla on 9/16/20.
+//  Copyright © 2020 Danielle Padilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
