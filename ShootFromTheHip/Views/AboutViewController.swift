4b9ba46fec6d26cdb4e69e0ba66fd7ca2da6f962
ayipadilla <danyelli@gmail.com>
2020-10-15 05:03:16 +0800

    Updated About View UI

--- ShootFromTheHip/Views/AboutViewController.swift
+++ ShootFromTheHip/Views/AboutViewController.swift
@@ -6,16 +6,23 @@
 //
 
 import UIKit
+import SafariServices
 
 class AboutViewController: UITableViewController {
   static let storyboardIdentifier = "AboutViewController"
   static let navControllerStoryboardIdentifier = "AboutNavigationController"
   
   @IBOutlet private weak var closeButton: UIBarButtonItem!
-
+  @IBOutlet private weak var appNameLabel: UILabel!
+  @IBOutlet private weak var appVersionLabel: UILabel!
+  @IBOutlet weak var iconCreditsTextView: UITextView!
+  
+  private let viewModel = AboutViewModel()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     setupView()
+    setupContent()
   }
   
   private func setupView() {
@@ -24,9 +31,30 @@ class AboutViewController: UITableViewController {
     
     navigationController?.navigationBar.shadowImage = UIImage()
     tableView.tableFooterView = UIView()
+    
+    iconCreditsTextView.delegate = self
+  }
+  
+  private func setupContent() {
+    appNameLabel.text = viewModel.appDisplayName
+    appVersionLabel.text = viewModel.appVersion
+    iconCreditsTextView.text = viewModel.iconCredits
   }
   
   @objc private func closeView(_ sender: Any) {
     dismiss(animated: true, completion: nil)
+  }
+  
+  func showWebView(_ URL: URL) {
+      let viewController = SFSafariViewController(url: URL)
+      viewController.modalPresentationStyle = .popover
+      present(viewController, animated: true, completion: nil)
+  }
+}
+
+extension AboutViewController: UITextViewDelegate {
+  func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+    showWebView(URL)
+    return false
   }
 }
---
+++ ShootFromTheHip/Views/AboutViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  AboutViewModel.swift
+//  ShootFromTheHip
+//
+//  Created by Ayi Padilla on 10/15/20.
+//
+
+import Foundation
+
+class AboutViewModel {
+  var appDisplayName: String {
+    if let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
+        return name
+    } else if let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String {
+        return name
+    }
+    return ""
+  }
+  
+  var appVersion: String {
+    if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+      return "Version \(version)"
+    }
+    return ""
+  }
+  
+  var iconCredits: String {
+    let icons = [
+      "Ionicons https://ionicons.com/", "Unicons https://iconscout.com/unicons"]
+    let text = icons.joined(separator: "\n")
+    return text
+  }
+}
