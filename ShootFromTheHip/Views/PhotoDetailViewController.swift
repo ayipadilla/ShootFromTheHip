841a390e39f8ea01b049e82600dab20b321d1239
ayipadilla <danyelli@gmail.com>
2020-10-14 22:38:57 +0800

    Added Share image / link

--- ShootFromTheHip/Views/PhotoDetailViewController.swift
+++ ShootFromTheHip/Views/PhotoDetailViewController.swift
@@ -12,6 +12,8 @@ class PhotoDetailViewController: UIViewController {
   @IBOutlet private weak var photoImageView: UIImageView!
   @IBOutlet private weak var photoImageHeight: NSLayoutConstraint!
   @IBOutlet private weak var closeButton: UIButton!
+  @IBOutlet private weak var shareButton: UIButton!
+  var shareImage: UIImage?
 
   let viewModel: PhotoDetailViewModel = PhotoDetailViewModel()
 
@@ -23,18 +25,34 @@ class PhotoDetailViewController: UIViewController {
   
   private func setupView() {
     closeButton.addTarget(self, action: #selector(closeButtonTapped(_:)), for: .touchUpInside)
+    shareButton.addTarget(self, action: #selector(shareButtonTapped(_:)), for: .touchUpInside)
   }
   
   private func setupPhotoImageView() {
     guard let photoImageUrl = viewModel.photoImageURL,
           let heightWidthRatio = viewModel.heightWidthRatio else { return }
     photoImageHeight.constant = UIScreen.main.bounds.width * CGFloat(heightWidthRatio)
-    photoImageView.sd_setImage(with: photoImageUrl, completed: nil)
+    photoImageView.sd_setImage(with: photoImageUrl) { [weak self] (image, error, cacheType, url) in
+      guard let self = self else { return }
+      self.shareImage = image
+    }
     view.layoutIfNeeded()
   }
   
   @objc private func closeButtonTapped(_ sender: Any) {
     dismiss(animated: true, completion: nil)
+  }
+  
+  @objc private func shareButtonTapped(_ sender: Any) {
+    var items: [Any] = []
+    if let photoImage = shareImage {
+      items.append(photoImage)
+    } else if let photoImageURL = viewModel.photoImageURL {
+      items.append(photoImageURL)
+    }
+    guard !items.isEmpty else { return }
+    let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+    present(activityViewController, animated: true)
   }
 }
 
